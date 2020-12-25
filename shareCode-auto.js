1aa06d08459fba70a8505d0ffd1758120740060f
Jone2008 <70353049+Jone2008@users.noreply.github.com>
2020-12-25 17:19:48 +0800

    Update shareCode-auto.js

--- shareCode-auto.js
+++ shareCode-auto.js
@@ -88,7 +88,7 @@ function create(path, name) {
         if (needAgain) return;
         const { message } = JSON.parse(data);
         const { msg } = JSON.parse(data);
-        $.log(`\n${message},${msg}\n${data}`);
+        $.log(`\n${message}\n${data}`);
         $.result.push(`${name}： ${message}，${msg}`);
       } catch (e) {
         $.logErr(e, resp);
